86ac63dde73006c55f1f17f4909e0e9291ed80a3
Shariq <shariq.mohammed01@gmail.com>
2024-01-22 11:25:17 -0600

    First Commit

---
+++ src/main/java/com/example/ride_sharing/RideSharingApplication.java
@@ -0,0 +1,13 @@
+package com.example.ride_sharing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RideSharingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RideSharingApplication.class, args);
+	}
+
+}
